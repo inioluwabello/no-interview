49034454bd4b7d6d692b1b85ac63468494b5a2ab
David <inioluwabellodavid@gmail.com>
2024-09-13 13:26:56 +0100

    5th commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-// app/api/auth/[...nextauth]/route.ts
+// app/api/auth/[...nextauth].ts
 
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
@@ -23,4 +23,6 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions);
+export default async function handler(req: any, res: any) {
+  return NextAuth(req, res, authOptions);
+}
